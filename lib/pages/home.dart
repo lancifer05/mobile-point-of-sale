7f5c063cd87f8237cb381edec258eda74afbaab2
Freelan Carganilla Gamboa <frca.gamboa.up@phinmaed.comclear>
2024-08-30 15:06:47 +0800

    responsiveness added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,28 +10,42 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    // Get the screen width to determine the Flex direction
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 350,
-              height: 150,
-              child: Card(
-                  child: Container(
-                    padding:EdgeInsets.all(50),
-                child: Column(
+      body: Flex(
+        direction: screenWidth < 600 ? Axis.vertical : Axis.horizontal,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              color: Colors.amber,
+              child: const Center(
+                child:  Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text("van ho ten"),
+                    Text("Foo", style: TextStyle(fontSize: 24)),
+                    Text("Bar", style: TextStyle(fontSize: 24)),
                   ],
                 ),
-              )),
+              ),
             ),
-
-            // Image(image: AssetImage("janedoe.jpg"))
-          ],
-        ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.lightBlue,
+              child: const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Item 1", style: TextStyle(fontSize: 24)),
+                    Text("Item 2", style: TextStyle(fontSize: 24)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
